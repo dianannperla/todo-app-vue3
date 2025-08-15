8ba3f8d066844f1b321f59fb856a79b53acc91b0
dianannperla <waafirmelyza@hotmail.com>
2025-08-15 05:48:59 +0100

    feat: initial commit for src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Danh sách công việc</h1>
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
